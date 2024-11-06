b35822cddc83118acd956f56cb259e7744d8678a
manyala1 <paulmuthama60@gmail.com>
2024-11-06 14:18:28 +0000

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+""" a basic flask app"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def hello():
+    """ render a basic html file """
     return render_template('0-index.html')
 
-if __name__ == '__man__':
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
