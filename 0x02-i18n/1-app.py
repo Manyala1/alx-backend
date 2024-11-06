2795023f7391368b0b1a2e6820e991263eb51fea
manyala1 <paulmuthama60@gmail.com>
2024-11-06 14:40:27 +0000

    py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" a basic flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = flask(__name__)
+
+class Config(object):
+    """ Config class fro Babel object"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def hello():
+    """ render a basic html file"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run()
